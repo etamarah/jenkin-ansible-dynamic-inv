e57c213d77cf33428310429b2d2d525d13f697bd
etamarah <111328876+etamarah@users.noreply.github.com>
2023-02-23 10:45:57 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,8 +4,8 @@ provider "aws" {
 resource "aws_instance" "app" {
     ami           = "ami-08e637cea2f053dfa"
     instance_type = "t3.micro"
-    security_groups = ["jkSG"]
-    key_name      =  "key29"
+    security_groups = ["default"]
+    key_name      =  "abymagoo"
     tags = {
         Name = " tomcat"
         Environment = "dev"
